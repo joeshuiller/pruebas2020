62b2a2deb11506f36079bfab6f9b0f86755548a5
joeshuiller <janes_saenz@hotmail.com>
2020-02-12 11:45:41 -0500

    Initial Commit

---
+++ Prueba/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Prueba
+//
+//  Created by Usuario on 12/02/20.
+//  Copyright © 2020 Janes saenz puerta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
